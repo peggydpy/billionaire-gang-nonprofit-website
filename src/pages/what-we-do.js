f1ff5a1704009a1d1841d07f16b5f0bfef625a68
peggydpy <peggyrudarnel@gmail.com>
2025-09-11 09:53:57 -0500

    Create what-we-do.js

---
+++ src/pages/what-we-do.js
@@ -0,0 +1,13 @@
+export default function WhatWeDo(){
+  return (
+    <main style={{maxWidth:960,margin:"0 auto",padding:32,fontFamily:"Arial, sans-serif"}}>
+      <h1 style={{fontSize:32,fontWeight:800}}>What We Do</h1>
+      <ul style={{marginTop:12,lineHeight:1.8}}>
+        <li>Water giveaways (H3O+)</li>
+        <li>Wellness studio & Le Yoga</li>
+        <li>Tech & game room access</li>
+        <li>Community events & campaigns</li>
+      </ul>
+    </main>
+  );
+}
