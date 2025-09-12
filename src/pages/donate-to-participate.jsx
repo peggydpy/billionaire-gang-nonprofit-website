6b67c6d7efe1cc1da081921cbfd4a8af19c00257
peggydpy <peggyrudarnel@gmail.com>
2025-09-12 17:51:01 -0500

    chore: remove pages/ donate route (migrated to app/)

--- src/pages/donate-to-participate.jsx
+++ src/pages/donate-to-participate.jsx
@@ -1,10 +1,1 @@
-// src/pages/donate-to-participate.jsx
-import ProductGrid from "../components/ProductGrid";
 
-export default function DonateToParticipatePage() {
-  return (
-    <main className="max-w-6xl mx-auto px-4 py-10">
-      <ProductGrid />
-    </main>
-  );
-}
