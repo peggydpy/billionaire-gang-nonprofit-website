a0fde28982698acb3af5918335300fb097185c26
peggydpy <peggyrudarnel@gmail.com>
2025-09-12 17:50:01 -0500

    Create page.jsx

---
+++ src/app/donate-to-participate/page.jsx
@@ -0,0 +1,9 @@
+import ProductGrid from "@/components/ProductGrid";
+
+export default function DonateToParticipatePage() {
+  return (
+    <main className="max-w-6xl mx-auto px-4 py-10">
+      <ProductGrid />
+    </main>
+  );
+}
