7fd081470950e945c79c1d87a5bd8b3920ca08e6
peggydpy <peggyrudarnel@gmail.com>
2025-09-11 21:28:36 -0500

    feat: show NavBar on all pages via _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 // src/pages/_app.js
 import '../styles/globals.css';
+import NavBar from '../components/NavBar';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
